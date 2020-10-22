744adf7e2fbd0569ca4270b202baee3504c6cb2a
Jordan Rocha <70rd23@gmail.com>
2020-10-22 16:57:36 -0300

    Added features to recive attack and update game state

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -6,4 +6,16 @@ defmodule ExMon.Game.Status do
     IO.inspect(Game.info())
     IO.puts("---------------------------")
   end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\n======== Invalide move: #{move}. ========\n")
+  end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n======== Player attacked computer, dealing #{damage} damage. ========\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n======== Computer attacked player, dealing #{damage} damage. ========\n")
+  end
 end
