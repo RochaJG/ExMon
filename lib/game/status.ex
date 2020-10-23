e39dcefdcdde347203b4615e057f2f7a8f9d656b
Jordan Rocha <70rd23@gmail.com>
2020-10-23 15:11:55 -0300

    Added heal action

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -28,4 +28,8 @@ defmodule ExMon.Game.Status do
   def print_move_message(:player, :attack, damage) do
     IO.puts("\n======== Computer attacked player, dealing #{damage} damage. ========\n")
   end
+
+  def print_move_message(player, :heal, life) do
+    IO.puts("\n======== The #{player} healled itsealf in #{life} life points. ========\n")
+  end
 end
